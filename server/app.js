2cc682ca8a88afe01aaf3f7fa32dd07632f650ae
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-04-12 20:49:47 +0530

    corrected pdf-parse version

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-const env = require("dotenv").config
+const env = require("dotenv").config()
 const express = require("express");
 const app = express();
+const uploadRoutes = require("./routes/uploadRoutes");
 
 app.get("/", (req, res)=>{
     res.send("This Home Page")
 })
+
+app.use("/api", uploadRoutes);
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const app = require("./app")
 const connectDB = require("./config/db")
 
 
-
-
 app.listen(process.env.PORT, ()=>{
     console.log("Server is Running")
 })
+
+connectDB()
