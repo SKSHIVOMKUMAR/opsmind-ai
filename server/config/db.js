1d4629e6bbe4aa08bbf3944f988da653d4749216
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-04-12 20:48:44 +0530

    correted a minor mistyped dot in config/db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connetDB(){
     try {
-        mongoose.connect("mongodb://localhost:27017/opsmind-Ai").
+        mongoose.connect("mongodb://localhost:27017/opsmind-Ai")
         console.log("Database is Connected")
     } catch (error) {
         console.log("Error", error);
